ed79c0f04c82c3ee30eb41e3e7c467dc7a3270dc
massaracsh7 <fatum7@gmail.com>
2024-11-01 13:00:23 +0300

    feat: add redux toolkit, imagetools

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,15 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { imagetools } from 'vite-imagetools'
 
-// https://vite.dev/config/
+// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  css: {
+    preprocessorOptions: {
+      scss: {
+        api: 'modern-compiler' // or "modern"
+      }
+    }
+  },
+  plugins: [react(), imagetools()],
 })
